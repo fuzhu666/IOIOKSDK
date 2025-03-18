e13241ee77bd0cf12a073ddebf9d5918ed604efa
mazeyu <mazeyu@zhizh.com>
2025-03-18 17:57:03 +0800

    fix

--- android/src/main/java/com/example/flutter_ioioksdk/FlutterIoioksdkPlugin.java
+++ android/src/main/java/com/example/flutter_ioioksdk/FlutterIoioksdkPlugin.java
@@ -1,20 +1,31 @@
 package com.example.flutter_ioioksdk;
 
-import androidx.annotation.NonNull;
+import android.app.Activity;
+import android.app.Application;
+import android.content.ComponentName;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
 
+import androidx.annotation.NonNull;
 import io.flutter.embedding.engine.plugins.FlutterPlugin;
+import io.flutter.embedding.engine.plugins.activity.ActivityAware;
+import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding;
 import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
+import com.dqpn.lib.*;
+import com.dqpn.net.bean.DQPNUInfo;
+import com.dqpn.net.util.DQPNNetCallback;
+import com.dqpn.other.ad.DQPNAd;
+import com.dqpn.other.ad.core.DQPNAppOpenListener;
 
 /** FlutterIoioksdkPlugin */
-public class FlutterIoioksdkPlugin implements FlutterPlugin, MethodCallHandler {
-  /// The MethodChannel that will the communication between Flutter and native Android
-  ///
-  /// This local reference serves to register the plugin with the Flutter Engine and unregister it
-  /// when the Flutter Engine is detached from the Activity
+public class FlutterIoioksdkPlugin implements FlutterPlugin, MethodCallHandler, ActivityAware {
   private MethodChannel channel;
+  private Activity activity;
+  private Application application;
+  private ApplicationInfo applicationInfo;
 
   @Override
   public void onAttachedToEngine(@NonNull FlutterPluginBinding flutterPluginBinding) {
@@ -22,10 +33,12 @@ public class FlutterIoioksdkPlugin implements FlutterPlugin, MethodCallHandler {
     channel.setMethodCallHandler(this);
   }
 
+
   @Override
   public void onMethodCall(@NonNull MethodCall call, @NonNull Result result) {
-    if (call.method.equals("getPlatformVersion")) {
-      result.success("Android " + android.os.Build.VERSION.RELEASE);
+    if (call.method.equals("initSDK")) {
+      System.out.println("cnmcnm===>1");
+      result.success(true);
     } else {
       result.notImplemented();
     }
@@ -34,5 +47,66 @@ public class FlutterIoioksdkPlugin implements FlutterPlugin, MethodCallHandler {
   @Override
   public void onDetachedFromEngine(@NonNull FlutterPluginBinding binding) {
     channel.setMethodCallHandler(null);
+  }
+
+  @Override
+  public void onAttachedToActivity(@NonNull ActivityPluginBinding binding) {
+    activity = binding.getActivity();
+    application = activity.getApplication();
+    try {
+      applicationInfo = application.getPackageManager().getApplicationInfo(application.getPackageName(), PackageManager.GET_META_DATA);
+      DQPNConfig
+        .getInstance()
+        .setAppKey(applicationInfo.metaData.getString("appkey"))
+        .setAppSecret(applicationInfo.metaData.getString("appSecret"))
+        .setAfDevKey(applicationInfo.metaData.getString("afDevKey"))
+        .setUmkAppId(applicationInfo.metaData.getString("umkAppid"))
+        .setServerUrl(applicationInfo.metaData.getString("serverUrl"))
+        .setStatUrl(applicationInfo.metaData.getString("statUrl"))
+        .setVideoUnitId(applicationInfo.metaData.getString("videoUnitId"))
+        .setAppOpenUnitId(applicationInfo.metaData.getString("openUnitId"))
+        .setInsUnitId(applicationInfo.metaData.getString("insUnitId"))
+        .setBannerUnitId(applicationInfo.metaData.getString("bannerUnitId"))
+        .setAdmobOpenUnitId(applicationInfo.metaData.getString("admobOpenUnitId"))
+        .setDebug(applicationInfo.metaData.getBoolean("debug"))
+        .setPrintLog(applicationInfo.metaData.getBoolean("log"));
+      DQPNLib.init(activity, new DQPNNetCallback<DQPNUInfo>() {
+        @Override
+        public void OnCompleted(boolean b, DQPNUInfo dqpnuInfo) {
+          System.out.println(dqpnuInfo.uid);
+            try {
+                Thread.sleep(5000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            DQPNAd.showAppOpen(DQPNConfig.getAdmobOpenUnitId(), new DQPNAppOpenListener() {
+              @Override
+              public void onFialed() {
+
+              }
+
+              @Override
+              public void onHide() {
+
+              }
+          });
+        }
+      });
+      DQPNLib.onApplicationCreate(application);
+    } catch (Exception e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  @Override
+  public void onDetachedFromActivityForConfigChanges() {
+  }
+
+  @Override
+  public void onReattachedToActivityForConfigChanges(@NonNull ActivityPluginBinding binding) {
+  }
+
+  @Override
+  public void onDetachedFromActivity() {
   }
 }
--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,10 +1,36 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'dart:async';
 
 import 'package:flutter/services.dart';
 import 'package:flutter_ioioksdk/flutter_ioioksdk.dart';
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await FlutterIoioksdk.initSDK(
+    debug: kDebugMode ? true : false,
+    log: true,
+    appkey: '2070C514A61F6E658A4A525E',
+    appSecret: '88DC852208FE4DEFB6C1691001211268',
+    serverUrl: 'https://hrt.jinchuchi.com',
+    statUrl: 'https://jka.jinchuchi.com',
+    iosAppId: '6740135195',
+    umkAppid: '1097563993230311424',
+    afDevKey: 'RfXDEDH92Sp4sYkR7kdvo3',
+    videoUnitId: '',
+    insUnitId: '',
+    openUnitId: 'ca-app-pub-3940256099942544/5575463023',
+    bannerUnitId: ''
+  );
+
+  print('cnmcnm');
+
+  Future.delayed(Duration(seconds: 5), () {
+    FlutterIoioksdk.showOpenAd((args) {
+      print('openAd: $args');
+    });
+  });
+
   runApp(const MyApp());
 }
 
